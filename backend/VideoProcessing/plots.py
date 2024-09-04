96e32773dc8e7204c34c0a8f58bdf74e787fff1e
Piotr Kacprzak <piotrkac4@gmail.com>
2024-09-04 15:20:39 +0200

    Generating plots depending on duration

--- backend/VideoProcessing/plots.py
+++ backend/VideoProcessing/plots.py
@@ -1,9 +1,10 @@
 from datetime import datetime, timedelta
-
 import matplotlib.pyplot as plt
 import pandas as pd
+from matplotlib import ticker
 
-def generate_plots(duration=None):
+
+def generate_plots(duration='Yesterday'):
     curr_datetime = datetime.now().date()
     today = curr_datetime.strftime("%d/%m/%Y")
 
@@ -12,18 +13,33 @@ def generate_plots(duration=None):
 
     df = df[(df['date'] < curr_datetime)]
 
+    fig, ax = plt.subplots(figsize=(12, 6))
+
     if duration == 'Yesterday':
         yesterday = datetime.now().date() - timedelta(days=1)
         df = df[df['date'] == yesterday]
+        ax.plot(df['time'], df['vehicles'])
+        ax.xaxis.set_major_locator(ticker.MultipleLocator(12))
     elif duration == 'One Week':
         one_week = datetime.now().date() - timedelta(days=8)
         df = df[df['date'] >= one_week]
+        df = df.groupby('date').agg({'vehicles':'mean'}).reset_index()
+
+        ax.plot(df['date'], df['vehicles'])
+        ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
     else:
         one_month = datetime.now().date() - timedelta(days=31)
         df = df[df['date'] >= one_month]
+        ax.plot(df['date'], df['vehicles'])
+        df = df.groupby('date').agg({'vehicles': 'mean'}).reset_index()
 
-    print(df.describe())
+        ax.plot(df['date'], df['vehicles'])
+        ax.xaxis.set_major_locator(ticker.MultipleLocator(7))
+
+    # plt.show()
+
+    return fig, ax
 
 
 
-generate_plots('One Month')
+generate_plots('One Week')
